d3aeb3e1d4a0677b394a7480e6bdf5c96fca6df4
PMBPG <pmbpk@yahoo.com>
2023-08-20 17:40:18 +0330

    remove node-modules and add event on project

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react'
 import style from './card.module.css';
 export default class Card extends Component {
+  constructor(){
+    super();
+    this.state ={
+      nums: 0,
+    }
+  }
+
+  upOne = () =>{
+    this.setState({
+      nums: this.state.nums + 1,
+     
+    })
+  }
+  downOne = () =>{
+    if(this.state.nums >= 1){
+      this.setState({
+        nums: this.state.nums - 1,
+      })  
+    }
+  }
+   
+
   render() {
     return (
       <div className={style.container}>
         <img src={this.props.image}/>
         <p>{this.props.name}</p>
-        <span>${this.props.price}</span>
+        <span className={style.spann}>${this.props.price}</span>
+        <div className={style.keys}>
+          <span className={this.state.nums ? "" : style.countDown} onClick={this.downOne}>-</span>
+          <div className={style.count}>{this.state.nums}</div>
+          <span onClick={this.upOne}>+</span>
+        </div>
       </div>
     )
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import style from './search.module.css'
-const Search = () => {
+import React, { Component } from 'react'
+import style from './search.module.css';
+export default class Search extends Component {
+    constructor(){
+        super();
+        this.state={
+            text: '',
+        }
+    }
+    changeHandler = event =>{
+        this.setState({
+            text: event.target.value,
+        })
+    }
+  render() {
     return (
         <div className={style.container}>
+            <div className={style.phoneSearch}>
             <h2>Search Phones</h2>
+            <h4>{this.state.text}</h4>
+            </div>
             <div className={style.searchBox}>
-            <input type='text' placeholder='Search' />
+            <input type='text'value={this.state.text} placeholder='Search' onChange={this.changeHandler} />
             <button>Search</button>
             </div>
         </div>
     );
-};
-
-export default Search;
+  }
+}
